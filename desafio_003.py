3f262942236d29e6c5e908caed26866dcf824683
Luiz Paulo <luizpaulodp88@gmail.com>
2026-08-16 18:43:52 -0300

    desafio 003

---
+++ desafio_003.py
@@ -0,0 +1,7 @@
+# ========== desafio 003 ==============
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+resulta = num1 + num2
+print(f'A soma de {num1} + {num2} = {resulta}')
