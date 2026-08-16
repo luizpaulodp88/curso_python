c9be60b996b3ab3bbb7dbfce6263bc8bd4c7e48d
Luiz Paulo <luizpaulodp88@gmail.com>
2026-08-16 18:33:01 -0300

    comentado o código

--- desafio_001.py
+++ desafio_001.py
@@ -1,4 +1,4 @@
-# ========== desafio 01 ==============
+# ========== desafio 001 ==============
 
 nome = input('Digite seu nome: ')
 print(f"Seja bem-vindo {nome}")
--- desafio_002.py
+++ desafio_002.py
@@ -1,3 +1,5 @@
+# ========== desafio 002 ==============
+
 dia = input('Digite o dia do seu nascimento: ')
 mes = input('Digite o mês do seu nascimento: ')
 ano = input('Digite o ano do seu nascimento: ')
